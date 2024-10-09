0871e44574faa4d10d8b5e66ab42f2e908d6754a
anubhavshrivastav <shrivastavanubhav0@gmail.com>
2024-10-09 13:49:06 +0530

    input handler

---
+++ 03_event-target/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+const [userInput, setInput] = useState('')
+
+ function inputHandler(){
+  console.log(event.target.value)
+  setInput(event.target.value)
+ }
+
+
+
+  return (
+    <>
+     <h1>input outt!</h1>
+       <input id='input' onChange={inputHandler} type="text" />
+      <h2>welcome {userInput}</h2>
+
+    </>
+  )
+}
+
+export default App
