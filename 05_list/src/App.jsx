32b831bd711642e0570fde4408b4c8251e31bdb9
anubhavshrivastav <shrivastavanubhav0@gmail.com>
2024-10-15 13:54:48 +0530

    add styling

--- 05_list/src/App.jsx
+++ 05_list/src/App.jsx
@@ -1,49 +1,49 @@
 import './App.css'
 
-// function App() {
+ function App() {
 
-//  let shoppingList = ["orange","apple","Eggs","rice","bread","candle","potato","jackFruits"];
+  let shoppingList = ["orange","apple","Eggs","rice","bread","candle","potato","jackFruits"];
 
 
-//   return (
-//     <>
-//       <h1>shopping list</h1>
-//      <ul>
-//      {
-//       shoppingList.map((item,index) => {
+   return (
+     <>
+       <h1>shopping list</h1>
+      <ul>
+      {
+       shoppingList.map((item,index) => {
 
-//        if(index % 2 != 0 ){
-//        return <li id='odd' >{item}</li>
-//        }
+        if(index % 2 != 0 ){
+        return <li id='odd' >{item}</li>
+        }
 
-//         return <li>{item}</li>
-//       })
-//      } 
-//         </ul>
-//     </>
-//   )
+         return <li style={{padding: "1rem"}}>{item}</li>
+       })
+      } 
+         </ul>
+     </>
+   )
+ }
+
+
+// function App(){
+
+// let userName = "dev";
+// let loggedIn = true;
+
+// function getUserMessage(){
+// if(loggedIn){
+//   return userName;
+// }else{
+//   return " please Login";
+// }
 // }
 
 
-function App(){
-
-let userName = "dev";
-let loggedIn = true;
-
-function getUserMessage(){
-if(loggedIn){
-  return userName;
-}else{
-  return " please Login";
-}
-}
-
-
-  return(
-    <>
-    <h1> welcome {getUserMessage()}</h1>
-    </>
-  )
-}
+//   return(
+//     <>
+//     <h1> welcome {getUserMessage()}</h1>
+//     </>
+//   )
+// }
 
 export default App
